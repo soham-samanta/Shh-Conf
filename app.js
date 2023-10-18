28b21fa1d27e45a890215dd4c0e30fc599d1caed
Soham Samanta <soham@Sohams-MacBook-Air-3.local>
2023-10-18 20:46:35 +0530

    update v3.2

--- app.js
+++ app.js
@@ -91,6 +91,8 @@ app.get("/auth/google",
   passport.authenticate('google', { scope: ["profile"] })
 );
 
+
+
 app.get("/auth/google/secrets",
   passport.authenticate('google', { failureRedirect: "/login" }),
   function(req, res) {
